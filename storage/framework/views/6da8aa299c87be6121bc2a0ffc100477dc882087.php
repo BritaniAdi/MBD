95221c64a12487b8bead6d1e0ac13e7b3b8fe85a
Britani A <72696767+BritaniAdi@users.noreply.github.com>
2023-05-12 00:25:30 +0700

    Add files via upload

---
+++ storage/framework/views/6da8aa299c87be6121bc2a0ffc100477dc882087.php
@@ -0,0 +1,24 @@
+
+<?php $__env->startSection('content'); ?>
+ 
+<div class="card">
+  <div class="card-header">Students Page</div>
+  <div class="card-body">
+      
+      <form action="<?php echo e(url('student')); ?>" method="post">
+        <?php echo csrf_field(); ?>
+
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Legacy</label></br>
+        <input type="text" name="legacy" id="legacy" class="form-control"></br>
+        <label>Class</label></br>
+        <input type="text" name="class" id="class" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('students.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\mbd\mbdapp\resources\views/students/create.blade.php ENDPATH**/ ?>
